7f08cf94051abafd4e54217355656c43b8ddea43
codebymikey <9484406+codebymikey@users.noreply.github.com>
2025-03-11 15:12:47 +0000

    Add better phpunit.xml script

--- .gitpod/utils/script-templates/ddev-phpunit.template.sh
+++ .gitpod/utils/script-templates/ddev-phpunit.template.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p "${GITPOD_REPO_ROOT}/web/sites/simpletest/browser_output"
-ddev exec_d phpunit -c /var/www/html/web/core/phpunit.xml.dist "$@"
+ddev exec_d phpunit -c /var/www/html/phpunit.xml "$@"
